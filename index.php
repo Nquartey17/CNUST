49e801fe1e6b26c7f200bec7539c980b0ac05aaa
Nquartey17 <68352586+Nquartey17@users.noreply.github.com>
2021-02-22 05:40:56 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+?php include('include/config.php'); ?>
+<link href="css/style.css" rel="stylesheet">
+
+
+<html>
+	<center><h1>CNU Student Helper</h1></center>
+</html>
+
+<a href="gpacalc.php">GPA calculator</a>
+<a href="finalgradecalc.php">Final Grade calculator</a>
